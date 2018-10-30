520db0ce63642094670a644e8883d2122b1d42d7
matinraayai <raayaiardakani.m@husky.neu.edu>
2018-10-30 08:34:21 -0400

    Removed data and model folders.

--- data/__init__.py
+++
@@ -1,3 +0,0 @@
-# dataset
-from .dataset import BasicDataset, PolaritySynapseDataset, AffinityDataset
-from .dataset import collate_fn, collate_fn_test
--- model/__init__.py
+++
@@ -1,6 +0,0 @@
-# libs
-# loss function
-from .loss import WeightedMSE
-
-# model
-from .model import unet_SE_synBN
